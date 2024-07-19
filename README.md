d62ca76b2c3841987e7e3346052d2764f6e9eec0
Orçun Çolak <orcun.colak@hazelcast.com>
2024-07-19 09:32:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Read me
+
+The original idea is from  
+https://medium.com/@ersinelmas17/mastering-api-gateways-with-java-and-spring-framework-a5ccfdf638e1
+
+# Global Filters
+
+These filters apply to all routes, allowing for functionalities like logging or authentication to be implemented
+universally.
+
+# Custom Route Predicates
+
+Developers can define their own criteria for how requests are routed, enabling fine-grained control over the request
+handling.
+
+# Rate Limiting
+
+Protect your services from being overwhelmed by too many requests, using built-in support for rate limiting.
---
+++ src/main/java/com/colak/springtutorial/config/GatewayConfig.java
@@ -0,0 +1,19 @@
+package com.colak.springtutorial.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    // http://localhost:8080/get
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/get")
+                        .uri("http://httpbin.org"))
+                .build();
+    }
+}
